4e91245e008f4b3b8c09c0bd94c6b0310c873d6e
sivakishor24 <58840775+sivakishor24@users.noreply.github.com>
2020-02-28 12:28:58 +0530

    Add files via upload

---
+++ mail.sh
@@ -0,0 +1,13 @@
+ADMIN="sivakishor024@gmail.com"
+# set alert level 90% is default
+ALERT=10
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $usep -ge $ALERT ]; then
+    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" | 
+     mail -s "Alert: Almost out of disk space $usep" $ADMIN
+  fi
+done
